cc7686b0c648a46888a097077069685860a287db
Nicholas Hwang <nick.joosung.hwang@gmail.com>
2012-05-03 22:21:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ArtGrabbr
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artgrabbr::Application.config.secret_token = 'da91bf244d2665386504999b42de8b1131f9bbf98cd32eeb12222bc49e754c2682215d0dd79586766903e9ea989ea9095e1f1ee23a96f77a5c38c69be5945611'
